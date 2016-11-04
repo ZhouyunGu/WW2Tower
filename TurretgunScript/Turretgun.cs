f7bf0fa8c0bd8f4d040ed2c690e60b710f0efb5b
jianbinsitu <jianbin.situ@gmail.com>
2016-11-03 21:59:45 -0400

    Add files via upload

---
+++ TurretgunScript/Turretgun.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Turretgun : MonoBehaviour {
+
+    public GameObject Bulletprefab;
+
+    void Start()
+    {       
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FireBullet()
+    {
+        GameObject Enemy = GameObject.FindWithTag("enemy");
+
+        if(Enemy != null)
+        {
+            GameObject bullet = (GameObject)Instantiate(Bulletprefab);
+
+            bullet.transform.position = transform.position;
+
+            Vector2 direction = Enemy.transform.position - bullet.transform.position;
+
+            bullet.GetComponent<BulletMovement>().SetDirection(direction);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("enemy"))
+        {
+            Invoke("FireBullet", 1f);
+        }
+    }
+}
