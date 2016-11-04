76f22118c276fd7b4bdb26d57a227f02a3253c18
lusterwater <calvinlu84@gmail.com>
2016-11-04 14:57:48 -0400

    Add files via upload

---
+++ Music-and-Music-Script/playBGMusic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class playBGMusic : MonoBehaviour {
+
+    public GameObject music;
+	void Start ()
+    {
+        GameObject musicTemp = GameObject.FindGameObjectWithTag("BGM");
+        if(musicTemp == null)
+        {
+            Instantiate(music, Vector3.zero, Quaternion.identity);
+        }
+	}
+	
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SceneManager.LoadScene("Scene 2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene("Scene 1");
+        }
+    }
+	
+}
