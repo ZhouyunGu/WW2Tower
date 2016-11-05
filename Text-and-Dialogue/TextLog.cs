c3f7bb0503082bca0f09c530fa519a01fd2b9306
lusterwater <calvinlu84@gmail.com>
2016-11-04 21:09:35 -0400

    Add files via upload

---
+++ Text-and-Dialogue/TextLog.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextLog : MonoBehaviour {
+
+    public GameObject Textbox;
+    public Text backgroundInfo;
+    public TextAsset textFile;
+    public string[] sentences;
+    public int currentLine;
+    public int endOfLine;
+
+	// Use this for initialization
+	void Start () {
+	    if(textFile != null)
+        {
+            sentences = (textFile.text.Split('\n'));
+        }
+
+        currentLine = 0;
+        endOfLine = sentences.Length-1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (currentLine <= endOfLine)
+        {
+            backgroundInfo.text = sentences[currentLine];
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine++;
+        }
+
+        if(currentLine > endOfLine)
+        {
+            Textbox.SetActive(false);
+        }
+	}
+}
