36aa56832151f838b413741bf70ac74a23b42aa0
jianbinsitu <jianbin.situ@gmail.com>
2016-11-06 11:50:59 -0500

    Add files via upload

---
+++ Turretgun/BulletMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour
+{
+    float speed;
+    Vector2 _direction;
+    bool isReady;
+    public int damage = 1;
+
+    void Awake()
+    {
+        speed = 10.0f;
+        isReady = false;
+    }
+
+    void Start()
+    {
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction.normalized;
+
+        isReady = true;
+    }
+
+    void Update()
+    {
+        if (isReady)
+        {
+            Vector2 position = transform.position;
+            position += _direction * speed * Time.deltaTime;
+            transform.position = position;
+        }
+        if(_direction.x == 0 && _direction.y == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Turretgun/Turretgun.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Turretgun : MonoBehaviour {
+    List<GameObject> enemy;
+    public GameObject Bulletprefab;
+
+    void Start()
+    {
+        enemy = new List<GameObject>();
+        InvokeRepeating("FireBullet", 1f, 0.5f);
+    }
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("enemy") && !enemy.Contains(other.gameObject))
+        {
+            enemy.Add(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("enemy") && enemy.Contains(other.gameObject))
+        {
+            enemy.Remove(other.gameObject);
+            if(other.gameObject != null)
+            {
+                enemy.Remove(other.gameObject);
+            }
+        }
+        if (other.CompareTag("TurretBullet"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    void FireBullet()
+    {
+        if (enemy[0] != null)
+        {
+            if (enemy[0].transform.position.x > (transform.position.x + 2))
+            {
+                enemy.Remove(enemy[0]);
+            }
+            GameObject bullet = (GameObject)Instantiate(Bulletprefab);
+
+            bullet.transform.position = transform.position;
+
+            Vector2 direction = enemy[0].transform.position - bullet.transform.position;
+
+            bullet.GetComponent<BulletMovement>().SetDirection(direction);
+
+            AudioSource firesound = gameObject.GetComponent<AudioSource>();
+            firesound.PlayOneShot(firesound.clip);
+        }
+        else
+        {
+            enemy.Remove(enemy[0]);
+        }
+    }
+}
