5e5a68583f5509a23b7bfbd7a0e34fc5314a409b
lusterwater <calvinlu84@gmail.com>
2016-11-27 20:27:21 -0500

    Add files via upload

---
+++ Text-and-Dialogue/UpgradeInTime.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpgradeInTime : MonoBehaviour {
+
+    public TextAsset uitText;
+    public Text uitInfo;
+    public GameObject uitObject;
+    public GameObject uitInstruction;
+    public string[] uitSentences;
+    public int uitCurrentLine;
+    public int uitEndOfLine;
+    public bool uitBool;
+
+    // Use this for initialization
+    void Start () {
+        uitBool = false;
+        uitObject.SetActive(uitBool);
+        uitInstruction.SetActive(uitBool);
+
+        if(uitText != null)
+        {
+            uitSentences = (uitText.text.Split('\n'));
+        }
+
+        uitCurrentLine = 0;
+        uitEndOfLine = uitSentences.Length - 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //change the false in the if statement to the score they need to get the first upgrade
+        if (true)
+        {
+            uitBool = true;
+            uitObject.SetActive(uitBool);
+            uitInstruction.SetActive(uitBool);
+        }
+
+        if(uitBool == true)
+        {
+            Time.timeScale = 0;
+            if(uitCurrentLine <= uitEndOfLine)
+            {
+                uitInfo.text = uitSentences[uitCurrentLine];
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                uitCurrentLine++;
+            }
+        }
+
+        if(uitCurrentLine > uitEndOfLine)
+        {
+            Time.timeScale = 1;
+            uitBool = false;
+            uitObject.SetActive(uitBool);
+            uitInstruction.SetActive(uitBool);
+        }
+	}
+}
