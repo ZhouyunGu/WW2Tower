3d2f1cd5e88257fb5396d5bd3a20a6ea4721709d
lusterwater <calvinlu84@gmail.com>
2016-12-02 14:01:49 -0500

    Add files via upload

---
+++ Music-and-Music-Script/VolumeController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeController : MonoBehaviour {
+
+    public void menuSetMasterVolume(float value)
+    {
+        AudioManager.instance.setMasterVolume(value);
+    }
+
+    public void menuSetMusicVolume(float value)
+    {
+        AudioManager.instance.setMusicVolume(value);
+    }
+
+    public void menuSetSfxVolume(float value)
+    {
+        AudioManager.instance.setSfxVolume(value);
+    }
+}
