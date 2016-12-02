739ca23ed97ecd4357af98b4e6dd2a98db419de9
lusterwater <calvinlu84@gmail.com>
2016-12-02 13:54:31 -0500

    Add files via upload

---
+++ Music-and-Music-Script/PlayBGM.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayBGM : MonoBehaviour {
+
+    public GameObject music;
+    public AudioClip Bgm;
+    void Start()
+    {
+        GameObject musicTemp = GameObject.FindGameObjectWithTag("BGM");
+        if (musicTemp == null)
+        {
+            Instantiate(music, Vector3.zero, Quaternion.identity);
+        }
+
+        AudioManager.instance.playMusic(Bgm);
+
+        DontDestroyOnLoad(music);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
